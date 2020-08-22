3b06104dcb55b077e859d8781aa802bfe2cf00c5
Hossein Kashfinia <hossein.kashfinia@gmail.com>
2020-08-22 09:09:02 +0430

    Updates

---
+++ precache-manifest.c9f5a93017b27265d54fe45501d53db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e64801ec57192936d94",
+    "url": "/mktodo-master/static/css/main.229f3b8f.chunk.css"
+  },
+  {
+    "revision": "4e64801ec57192936d94",
+    "url": "/mktodo-master/static/js/main.4e64801e.chunk.js"
+  },
+  {
+    "revision": "a4a073cd92f88f173b1b",
+    "url": "/mktodo-master/static/js/1.a4a073cd.chunk.js"
+  },
+  {
+    "revision": "dd45205b86dc1ce08f1d",
+    "url": "/mktodo-master/static/js/runtime~main.dd45205b.js"
+  },
+  {
+    "revision": "fc96841419f73d5f6cc338f27503a599",
+    "url": "/mktodo-master/index.html"
+  }
+];
